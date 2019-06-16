dee06389a2568889aad68bd848f5cffa2632fc9c
ajitsase <34818235+ajitsase@users.noreply.github.com>
2019-06-17 00:44:30 +0530

    Create the app for findAll() method in data jpa.

---
+++ src/main/java/com/app/SpringBootDataJpaFindAllMethodApplication.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDataJpaFindAllMethodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDataJpaFindAllMethodApplication.class, args);
+	}
+
+}
